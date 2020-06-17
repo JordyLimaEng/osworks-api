7e935c1681983ac38096784d38b14bd2047a54e4
JordyLimaEng <jordy.sousa@eng.ci.ufpb.br>
2020-06-17 19:06:16 -0300

    criação de repositorio e mysql + rest controllers

---
+++ src/main/java/com/jordy/osworks/api/controller/ClienteController.java
@@ -0,0 +1,31 @@
+package com.jordy.osworks.api.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jordy.osworks.domain.model.Cliente;
+
+@RestController
+public class ClienteController {
+	
+	@GetMapping("/clientes")
+	public List<Cliente> listar() {
+		var cliente1 = new Cliente();
+		cliente1.setId(1L);
+		cliente1.setNome("João");
+		cliente1.setTelefone("83 9 86695447");
+		cliente1.setEmail("joao@gmail.com");
+		
+		var cliente2 = new Cliente();
+		cliente2.setId(1L);
+		cliente2.setNome("Maria");
+		cliente2.setTelefone("83 9 88167988");
+		cliente2.setEmail("maria@gmail.com");
+		
+		return Arrays.asList(cliente1, cliente2);
+	}
+
+}
