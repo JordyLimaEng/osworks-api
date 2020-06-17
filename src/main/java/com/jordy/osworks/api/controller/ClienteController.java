581a47bd64a6e9a498be4e761087b5a40c734558
JordyLimaEng <jordy.sousa@eng.ci.ufpb.br>
2020-06-17 19:55:57 -0300

    método adicionar ao bd

--- src/main/java/com/jordy/osworks/api/controller/ClienteController.java
+++ src/main/java/com/jordy/osworks/api/controller/ClienteController.java
@@ -7,32 +7,44 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jordy.osworks.domain.model.Cliente;
 import com.jordy.osworks.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
 	@Autowired
 	private ClienteRepository clienteRepository;
 
-	@GetMapping("/clientes")
+	@GetMapping
 	public List<Cliente> listar() {
 		return clienteRepository.findAll(); // com repository já podemos retornar uma lista com os clientes sem
 											// necessidade de códigos verbosos
 	}
-	
-	@GetMapping("/clientes/{clienteId}")
+
+	@GetMapping("/{clienteId}")
 	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
-		Optional <Cliente> cliente = clienteRepository.findById(clienteId);		
-		
-		if(cliente.isPresent()) {// se houver obj na resposta
-			return ResponseEntity.ok(cliente.get()); //retorna obj e código 200 - ok
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+
+		if (cliente.isPresent()) {// se houver obj na resposta
+			return ResponseEntity.ok(cliente.get()); // retorna obj e código 200 - ok
 		}
-		
+
 		return ResponseEntity.notFound().build();// se a resposta não tiver nada, retorna código 404 - not found
+	}
+
+	@PostMapping
+	public Cliente adicionar(@RequestBody Cliente cliente) {// requestBody mapeia com o spring que o corpo que vai vir
+															// da requisição post vai ser do tipo cliente
+		
+		return clienteRepository.save(cliente);
+
 	}
 
 }
