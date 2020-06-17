3a5b3585f889e9a1a0ff78927eec2aab79f51284
JordyLimaEng <jordy.sousa@eng.ci.ufpb.br>
2020-06-17 19:46:44 -0300

    implementação da requisição para buscar e listar clientes

--- src/main/java/com/jordy/osworks/api/controller/ClienteController.java
+++ src/main/java/com/jordy/osworks/api/controller/ClienteController.java
@@ -1,9 +1,11 @@
 package com.jordy.osworks.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jordy.osworks.domain.model.Cliente;
@@ -19,6 +21,12 @@ public class ClienteController {
 	public List<Cliente> listar() {
 		return clienteRepository.findAll(); // com repository já podemos retornar uma lista com os clientes sem
 											// necessidade de códigos verbosos
+	}
+	
+	@GetMapping("/clientes/{clienteId}")
+	public Cliente buscar(@PathVariable Long clienteId) {
+		Optional <Cliente> cliente = clienteRepository.findById(clienteId);		
+		return cliente.orElse(null);// se a resposta não tiver nada, retorna null
 	}
 
 }
--- src/main/java/com/jordy/osworks/domain/repository/ClienteRepository.java
+++ src/main/java/com/jordy/osworks/domain/repository/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.jordy.osworks.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,8 @@ import com.jordy.osworks.domain.model.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+	
+	List<Cliente> findByNome(String nome); //executa find por nome
+	List<Cliente> findByNomeContaining(String nome); //executa find procurando por nomes que contenham a string especifica
 
 }
