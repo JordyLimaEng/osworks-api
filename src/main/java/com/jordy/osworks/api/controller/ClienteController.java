eb0f646f68c02c443c0120dd61cfa459fb92afc2
JordyLimaEng <jordy.sousa@eng.ci.ufpb.br>
2020-06-17 20:04:08 -0300

    implementação de adicionar retornando 201 - created

--- src/main/java/com/jordy/osworks/api/controller/ClienteController.java
+++ src/main/java/com/jordy/osworks/api/controller/ClienteController.java
@@ -4,12 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jordy.osworks.domain.model.Cliente;
@@ -40,6 +42,7 @@ public class ClienteController {
 	}
 
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)// pode responder o código HTTP pelo proprio annotation
 	public Cliente adicionar(@RequestBody Cliente cliente) {// requestBody mapeia com o spring que o corpo que vai vir
 															// da requisição post vai ser do tipo cliente
 		
