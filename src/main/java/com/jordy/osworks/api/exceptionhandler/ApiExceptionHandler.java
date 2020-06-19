589058258d3e4c2ba03263612d99a0eec16d3bbb
JordyLimaEng <jordy.sousa@eng.ci.ufpb.br>
2020-06-19 00:32:03 -0300

    refatorando resposta

--- src/main/java/com/jordy/osworks/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/jordy/osworks/api/exceptionhandler/ApiExceptionHandler.java
@@ -13,27 +13,42 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.jordy.osworks.domain.NegocioException;
+
 @ControllerAdvice // especifica que vai fazer os exceptions dos controladores
-public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
-	
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
 	@Autowired
 	private MessageSource messageSource;
-	
+
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<Object> handleNegocio(NegocioException ex, WebRequest request) {
+		var status = HttpStatus.BAD_REQUEST;
+		
+		var problema = new Problema();
+		problema.setStatus(status.value());
+		problema.setTitulo(ex.getMessage());
+		problema.setDataHora(LocalDateTime.now());
+		
+		return handleExceptionInternal(ex, problema, new HttpHeaders(), status, request);
+	}
+
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 		var campos = new ArrayList<Problema.Campo>();
-		
-		for(ObjectError error : ex.getBindingResult().getAllErrors()) {
-			String nome = ((FieldError)error).getField();
+
+		for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+			String nome = ((FieldError) error).getField();
 			String mensagem = messageSource.getMessage(error, LocaleContextHolder.getLocale());
-			
+
 			campos.add(new Problema.Campo(nome, mensagem));
-		}		
-		
+		}
+
 		var problema = new Problema();
 		problema.setStatus(status.value());
 		problema.setTitulo("Um ou mais campos estão inválidos, corrija e tente novamente");
