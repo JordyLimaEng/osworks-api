a9ac3ccc04de58b1dd791f727009241c70b00d67
JordyLimaEng <jordy.sousa@eng.ci.ufpb.br>
2020-06-19 00:35:46 -0300

    annotation para não responder null no body da request

--- src/main/java/com/jordy/osworks/api/exceptionhandler/Problema.java
+++ src/main/java/com/jordy/osworks/api/exceptionhandler/Problema.java
@@ -3,6 +3,10 @@ package com.jordy.osworks.api.exceptionhandler;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)// se um campo estiver NULL não retorna no body de erro
 public class Problema {
 	private Integer status;
 	private LocalDateTime dataHora;
